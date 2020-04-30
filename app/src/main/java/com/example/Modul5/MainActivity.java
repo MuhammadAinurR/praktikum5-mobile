76c90719eceba18f44b2fd0f038871880ab2537f
MuhammadAinurR <ainurmoh@gmail.com>
2020-04-30 19:25:28 +0700

    Praktikum Mobile Modul 5

--- app/src/main/java/com/example/Modul5/MainActivity.java
+++ app/src/main/java/com/example/Modul5/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.projectpeta;
+package com.example.Modul5;
 
 import androidx.fragment.app.FragmentActivity;
 
@@ -39,25 +39,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                 .snippet("Pelabuhan PPP Mayangan, Wisata Primadona, Mangunharjo, Mayangan, Mangunharjo, Kec. Mayangan, Kota Probolinggo, Jawa Timur 67217"));
         mapAPI.moveCamera(CameraUpdateFactory.newLatLng(bjbr));
 
-        LatLng bentar = new LatLng(-7.779307, 113.276679);
-        mapAPI.addMarker(new MarkerOptions().position(bentar).title("Wisata Pantai Bentar Probolinggo")
-                .snippet("Jalur Pantura Mayangan, Karang Anyar, Curahsawo, Kec. Gending, Probolinggo, Jawa Timur 67211"));
-        mapAPI.moveCamera(CameraUpdateFactory.newLatLng(bentar));
 
-        LatLng gili = new LatLng(-7.679147, 113.245678);
-        mapAPI.addMarker(new MarkerOptions().position(gili).title("Wisata Pantai Gili Ketapang ")
-                .snippet("Sapia, Gili Ketapang, Kec. Sumberasih, Probolinggo, Jawa Timur"));
-        mapAPI.moveCamera(CameraUpdateFactory.newLatLng(gili));
-
-        LatLng mada = new LatLng(-7.853030, 113.007900);
-        mapAPI.addMarker(new MarkerOptions().position(mada).title("Air Terjun Madakaripura")
-                .snippet("Sapih, Branggah, Lumbang, Probolinggo, Jawa Timur 67183"));
-        mapAPI.moveCamera(CameraUpdateFactory.newLatLng(mada));
-
-        LatLng myhouse = new LatLng(-7.734760, 113.120930);
-        mapAPI.addMarker(new MarkerOptions().position(myhouse).title("Rumahku Tersayang")
-                .snippet("Sumberan, Dungun, Kec. Tongas, Probolinggo, Jawa Timur 67252"));
-        mapAPI.moveCamera(CameraUpdateFactory.newLatLng(myhouse));
 
     }
 }
